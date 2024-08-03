807148386f08d40b5c6b236b96eb36c86494a0fe
Yoonji Na <yjna2316@naver.com>
2024-08-03 21:54:48 +0900

    init project

---
+++ src/main/kotlin/com/kopring/study_logs/StudyLogsApplication.kt
@@ -0,0 +1,11 @@
+package com.kopring.study_logs
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class StudyLogsApplication
+
+fun main(args: Array<String>) {
+	runApplication<StudyLogsApplication>(*args)
+}
---
+++ src/test/kotlin/com/kopring/study_logs/StudyLogsApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.kopring.study_logs
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class StudyLogsApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
